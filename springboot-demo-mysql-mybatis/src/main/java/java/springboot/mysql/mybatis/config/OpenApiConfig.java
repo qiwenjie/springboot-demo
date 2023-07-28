84c06051ede04c3e97503ed6c4bc44de23ce0fc1
qiwenjie <qiwenjie@cai-inc.com>
2023-07-28 14:56:38 +0800

    init

--- springboot-demo-mysql-mybatis/src/main/java/java/springboot/mysql/mybatis/config/OpenApiConfig.java
+++ springboot-demo-mysql-mybatis/src/main/java/java/springboot/mysql/mybatis/config/OpenApiConfig.java
@@ -6,7 +6,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
-import springboot.mysql.mybatis.constants.ResponseStatus;
 import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
@@ -22,6 +21,7 @@ import springfox.documentation.service.Response;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 
+import java.springboot.mysql.mybatis.constants.ResponseStatus;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
